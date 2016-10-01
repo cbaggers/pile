d52603c76cf66ea37d6d5baccb469884ec0950a6
Baggers <chris.bagley@gmail.com>
2016-10-01 23:50:51 +0200

    reduce consing more

--- render-ui.lisp
+++ render-ui.lisp
@@ -3,99 +3,105 @@
 ;;----------------------------------------------------------------------
 ;;
 
-(defun %render-ui (context)
-  (let* ((nk-ctx (pile-nk-ptr context))
-         (root-elem (pile-root context))
-         (render-data (root-element-render-data root-elem)))
-    (cepl:with-viewport (viewport render-data)
-      (gl:blend-equation :func-add)
-      (gl:blend-func :src-alpha :one-minus-src-alpha)
-      (gl:enable :scissor-test :blend)
-      (gl:disable :cull-face :depth-test)
-      ;;
-      (with-foreign-object (vert-layout '(:struct nk-draw-vertex-layout-element) 4)
-        ;;
-        (setf (mem-aref vert-layout '(:struct nk-draw-vertex-layout-element) 0)
-              `(attribute ,nk-vertex-position
-                          raw-bindings-nuklear::format ,nk-format-float
-                          offset 0))
-        ;;
-        (setf (mem-aref vert-layout '(:struct nk-draw-vertex-layout-element) 1)
-              `(attribute ,nk-vertex-texcoord
-                          raw-bindings-nuklear::format ,nk-format-float
-                          offset 8))
-        ;;
-        (setf (mem-aref vert-layout '(:struct nk-draw-vertex-layout-element) 2)
-              `(attribute ,nk-vertex-color
-                          raw-bindings-nuklear::format ,nk-format-r8g8b8a8
-                          offset 16))
-        ;;
-        (setf (mem-aref vert-layout '(:struct nk-draw-vertex-layout-element) 3)
-              `(attribute ,nk-vertex-attribute-count
-                          raw-bindings-nuklear::format ,nk-format-count
-                          offset 0))
-        ;;
-        (with-foreign-object (config '(:struct nk-convert-config))
-          (zero-out config '(:struct nk-convert-config))
-          (with-foreign-slots
-              ((vertex-layout
-                vertex-size vertex-alignment raw-bindings-nuklear::null
-                circle-segment-count curve-segment-count arc-segment-count
-                global-alpha shape-aa line-aa) config (:struct nk-convert-config))
-            (setf vertex-layout vert-layout
-                  vertex-size 20
-                  vertex-alignment 1
-                  raw-bindings-nuklear::null (null-tex render-data)
-                  circle-segment-count 22
-                  curve-segment-count 22
-                  arc-segment-count 22
-                  global-alpha 1s0
-                  shape-aa 0
-                  line-aa 0))
-          (cepl:with-gpu-array-as-pointer (vert-ptr (vert-array render-data)
-                                                    :access-type :write-only
-                                                    :target :array-buffer)
-            (cepl:with-gpu-array-as-pointer (elem-ptr (elem-array render-data)
-                                                      :access-type :write-only
-                                                      :target :element-array-buffer)
-              (with-foreign-objects ((v-buf '(:struct nk-buffer))
-                                     (e-buf '(:struct nk-buffer)))
-                (nk-buffer-init-fixed
-                 v-buf vert-ptr (garr-size (vert-array render-data)))
-                (nk-buffer-init-fixed
-                 e-buf elem-ptr (garr-size (elem-array render-data)))
-                (nk-convert nk-ctx (cmds render-data) v-buf e-buf config))))))
+(defvar *vert-layout*
+  (let ((vl (foreign-alloc '(:struct nk-draw-vertex-layout-element) :count 4)))
+    (setf (mem-aref vl '(:struct nk-draw-vertex-layout-element) 0)
+          `(attribute ,nk-vertex-position
+                      raw-bindings-nuklear::format ,nk-format-float
+                      offset 0))
+    (setf (mem-aref vl '(:struct nk-draw-vertex-layout-element) 1)
+          `(attribute ,nk-vertex-texcoord
+                      raw-bindings-nuklear::format ,nk-format-float
+                      offset 8))
+    (setf (mem-aref vl '(:struct nk-draw-vertex-layout-element) 2)
+          `(attribute ,nk-vertex-color
+                      raw-bindings-nuklear::format ,nk-format-r8g8b8a8
+                      offset 16))
+    (setf (mem-aref vl '(:struct nk-draw-vertex-layout-element) 3)
+          `(attribute ,nk-vertex-attribute-count
+                      raw-bindings-nuklear::format ,nk-format-count
+                      offset 0))
+    vl))
 
-      (let* ((vsize (cepl:viewport-resolution (cepl:current-viewport)))
-             (vw (v:x vsize))
-             (vh (v:y vsize))
-             (ortho (cepl:m! (/ 2 vw)  0          0 -1
-                             0         (/ -2 vh)  0  1
-                             0         0         -1  0
-                             0         0          0  1))
-             (scale-x 1s0)
-             (scale-y 1s0)
-             (offset 0))
-        (with-slots (vert-stream) render-data
-          (with-foreign-objects ((cmd '(:struct nk-draw-command)))
-            (setf cmd (nk--draw-begin nk-ctx (cmds render-data)))
-            (loop :while (not (null-pointer-p cmd)) :do
-               (with-foreign-slots ((elem-count) cmd (:struct nk-draw-command))
-                 (let ((clip-rect (foreign-slot-pointer cmd '(:struct nk-draw-command) 'clip-rect)))
-                   (when (> elem-count 0)
-                     (with-foreign-slots ((x y w h) clip-rect (:struct nk-rect))
-                       (gl:scissor (* x scale-x) (* (- vh (+ y h)) scale-y)
-                                   (* w scale-x) (* h scale-y)))
-                     (setf (cepl:buffer-stream-length vert-stream) elem-count)
-                     (setf (cepl.types::buffer-stream-start vert-stream) offset)
-                     (cepl:map-g #'pile.renderer:nk-basic
-                                 vert-stream
-                                 :tex (font-sampler render-data)
-                                 :proj-mtx ortho)
-                     (incf offset elem-count))))
-               (setf cmd (nk--draw-next cmd (cmds render-data) nk-ctx))))))
-      ;;
-      (gl:disable :scissor-test :blend)
-      (gl:enable :depth-test :cull-face))))
+(let ((ortho (cepl:m! 0  0  0 -1
+                      0  0  0  1
+                      0  0 -1  0
+                      0  0  0  1)))
+  (defun %render-ui (context)
+    (declare (optimize speed))
+    (let* ((nk-ctx (pile-nk-ptr context))
+           (root-elem (pile-root context))
+           (render-data (root-element-render-data root-elem)))
+      (cepl:with-viewport (viewport render-data)
+        (gl:blend-equation :func-add)
+        (gl:blend-func :src-alpha :one-minus-src-alpha)
+        (gl:enable :scissor-test :blend)
+        (gl:disable :cull-face :depth-test)
+        ;;
+        (let ((vert-layout *vert-layout*))
+          (with-foreign-object (config '(:struct nk-convert-config))
+            (zero-out config '(:struct nk-convert-config))
+            (with-foreign-slots
+                ((vertex-layout
+                  vertex-size vertex-alignment raw-bindings-nuklear::null
+                  circle-segment-count curve-segment-count arc-segment-count
+                  global-alpha shape-aa line-aa) config (:struct nk-convert-config))
+              (setf vertex-layout vert-layout
+                    vertex-size 20
+                    vertex-alignment 1
+                    raw-bindings-nuklear::null (null-tex render-data)
+                    circle-segment-count 22
+                    curve-segment-count 22
+                    arc-segment-count 22
+                    global-alpha 1s0
+                    shape-aa 0
+                    line-aa 0))
+            (cepl:with-gpu-array-as-pointer (vert-ptr (vert-array render-data)
+                                                      :access-type :write-only
+                                                      :target :array-buffer)
+              (cepl:with-gpu-array-as-pointer (elem-ptr (elem-array render-data)
+                                                        :access-type :write-only
+                                                        :target :element-array-buffer)
+                (with-foreign-objects ((v-buf '(:struct nk-buffer))
+                                       (e-buf '(:struct nk-buffer)))
+                  (nk-buffer-init-fixed
+                   v-buf vert-ptr (garr-size (vert-array render-data)))
+                  (nk-buffer-init-fixed
+                   e-buf elem-ptr (garr-size (elem-array render-data)))
+                  (nk-convert nk-ctx (cmds render-data) v-buf e-buf config)))))
+          (let* ((vsize (cepl:viewport-resolution (cepl:current-viewport)))
+                 (vw (v:x vsize))
+                 (vh (v:y vsize))
+                 (scale-x 1)
+                 (scale-y 1)
+                 (offset 0))
+            (declare (type (single-float 0s0 #.(float #xffff 0s0)) vw vh))
+
+            (setf (m4:melm ortho 0 0) (the single-float (/ 2s0 vw)))
+            (setf (m4:melm ortho 1 1) (/ -2s0 vh))
+
+            (with-slots (vert-stream) render-data
+              (with-foreign-objects ((cmd '(:struct nk-draw-command)))
+                (setf cmd (nk--draw-begin nk-ctx (cmds render-data)))
+                (loop :while (not (null-pointer-p cmd)) :do
+                   (with-foreign-slots ((elem-count (:pointer clip-rect)) cmd
+                                        (:struct nk-draw-command))
+                     (when (> elem-count 0)
+                       (with-foreign-slots ((x y w h) clip-rect (:struct nk-rect))
+                         (declare (type (single-float 0s0 #.(float #xffff 0s0))
+                                        x y w h))
+                         (gl:scissor (* x scale-x) (* (- vh (+ y h)) scale-y)
+                                     (* w scale-x) (* h scale-y)))
+                       (setf (cepl:buffer-stream-length vert-stream) elem-count)
+                       (setf (cepl.types::buffer-stream-start vert-stream) offset)
+                       (cepl:map-g #'pile.renderer:nk-basic
+                                   vert-stream
+                                   :tex (font-sampler render-data)
+                                   :proj-mtx ortho)
+                       (incf offset elem-count)))
+                   (setf cmd (nk--draw-next cmd (cmds render-data) nk-ctx))))))
+          ;;
+          (gl:disable :scissor-test :blend)
+          (gl:enable :depth-test :cull-face))))))
 
 ;;----------------------------------------------------------------------
