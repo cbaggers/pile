2e4ed75a21b0749ca5df0755b5c7f886fbbfd769
baggers <baggers@cave>
2016-10-02 20:09:06 +0200

    use key args internally in defui
    The reason is that then it is much for forgiving to changes to the definition

--- ui-compiling-macros.lisp
+++ ui-compiling-macros.lisp
@@ -1,13 +1,15 @@
 (in-package :pile)
-
+(in-readtable :fn.reader)
 
 (defmacro defui (name args &body body)
   (let* ((func-name (intern (format nil "%~a" name) (symbol-package name)))
-         (arg-names (mapcar (lambda (x) (first (ensure-list x))) args)))
+         (arg-names (mapcar λ(first (ensure-list _)) args))
+         (m-args (mapcan λ(list (intern (symbol-name _) :keyword) _)
+                         arg-names)))
     `(progn
        ;;
        (defmacro ,name ,(when args `(&key ,@args))
-         (append (list ',func-name ',+ctx+ ,@arg-names)))
+         (list ',func-name ',+ctx+ ,@m-args))
        ;;
-       (defun ,func-name (,+ctx+ ,@arg-names)
+       (defun ,func-name (,+ctx+ &key ,@args &allow-other-keys)
          ,@body))))
